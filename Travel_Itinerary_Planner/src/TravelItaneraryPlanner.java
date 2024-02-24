de81d850f0c1e22e7ca43f498ff97753229c2fb3
Madhubhashana Mahindarathne <104136158+Madhubhashana99@users.noreply.github.com>
2024-02-24 22:18:53 +0530

    update

--- Travel_Itinerary_Planner/src/TravelItaneraryPlanner.java
+++ Travel_Itinerary_Planner/src/TravelItaneraryPlanner.java
@@ -1,2 +1,24 @@
+import java.util.Scanner;
+
 public class TravelItaneraryPlanner {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Welcome to the Travel Itanerary Planner");
+
+        //Getting the user inputs
+        System.out.println("Enter your destination: ");
+        String destination = scanner.nextLine();
+
+        System.out.println("Enter the start date (YYYY-MM-DD): ");
+        String sd = scanner.nextLine();
+
+        System.out.println("Enter the end date (YYYY-MM-DD): ");
+        String ed = scanner.nextLine();
+
+        System.out.println("Enter your budget for the trip: ");
+        Double budget = scanner.nextDouble();
+
+    }
 }
